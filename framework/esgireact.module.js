29640794a1197427e2ec1db99913e2e8aaa69ced
aitamara <aitamarar@gmail.com>
2021-01-22 18:45:18 +0100

    MAJ moduleesgi

--- framework/esgireact.module.js
+++ framework/esgireact.module.js
@@ -1,18 +1,16 @@
 import * as Utils from './utils.module';
 
-class Component
-{
-    constructor(props, content, children)
-    {
+class Component {
+    constructor(props, content, children) {
 
         /**
-         * 
-         * 
-         * 
-         * TODO 
+         *
+         *
+         *
+         * TODO
          * VALIDER LES DONNEES ENTRANTES D'UNE FAÇON OU D'UNE AUTRE
-         * 
-         * 
+         *
+         *
          */
 
         // Variable privée
@@ -21,19 +19,17 @@ class Component
         var id = Utils.getUnsecureID();
 
         /** State courant du component */
-        var currentState = new State(this.id, props, content);
+        var currentState = new State(this.id, props, content, children);
 
         /** Mémoire du component */
         var oldState = {};
 
-        /** Enfant du component */
-        var children = children;
     }
 
     /**
      * Check si le nouveau state est égale à l'ancien
-     * @param {*} oldState 
-     * @param {*} newState 
+     * @param {*} oldState
+     * @param {*} newState
      * @returns bool
      */
     shouldUpdate(oldState, newState) {
@@ -44,31 +40,30 @@ class Component
 
     /**
      * Retourne un component avec les props données en paramètre
-     * @param {*} newProps 
-     * @param {*} content 
+     * @param {*} newProps
+     * @param {*} content
      * @returns
      */
     display(newState) {
 
-                /**
-         * 
-         * 
-         * 
-         * TODO 
-         * VALIDER LES DONNEES ENTRANTES D'UNE FAÇON OU D'UNE AUTRE (se baser sur la 
-         * validation dans le constructeur)
-         * 
-         * 
-         */
-        
+/**
+ *
+ *
+ *
+ * TODO
+ * VALIDER LES DONNEES ENTRANTES D'UNE FAÇON OU D'UNE AUTRE (se baser sur la
+ * validation dans le constructeur)
+ *
+ *
+ */
+
         // Affectation de l'ancien state avec la valeur du state courant pour sauvegarde
         this.setOldState(this.getCurrentState());
 
         // Vérifier si le state de l'application est égale aux new state envoyés
-        if (this.shouldUpdate(this.getOldState(), newState))
-        {
+        if (this.shouldUpdate(this.getOldState(), newState)) {
             // On demande une mise à jour du component
-            return this.render(this, this.getCurrentState());
+            return this.render(this, newState);
         }
 
         return this;
@@ -76,29 +71,35 @@ class Component
 
     /**
      * Mise à jour un component et de ses childrens
-     * @param {*} component 
-     * @param {*} state 
-     * @param {*} content 
+     * @param {*} this
+     * @param {*} state
+     * @param {*} content
      */
-    render(component, state) {
+    render(state) {
 
+        /**
+         *
+         * TODO
+         *
+         * THROW ERROR AU TEST
+         */
         // Si le props n'est pas bon, on return le component directement
-        if (component.getId() !== state.getComponentId()) return component;
+        if (this.getId() !== state.getComponentId()) return this;
 
         // Si oui, on affecte les newprops au currentState
         this.setCurrentState(state);
 
         // Si le component a des enfants
-        if (!!this.children) {
+        if (!!this.oldState.getChildren()) {
             // Pour chacun d'entre eux
-            for (let child of component.children) {
-               // Récupération des props
-                const childNewProps = getPropsByComponentId(child.getId(), state.getComponentId());
+            for (let child of this.oldState.getChildren()) {
+                // Récupération des props
+                const locatedChildren = getPropsByComponentId(child.getId(), state.getChildren());
 
                 // Si les props existent
-                if (!!childNewProps) {
+                if (!!locatedChildren) {
                     // on passe le relais à la méthode de l'enfant pour qu'il se mette à jour ou non
-                    child = child.display(childNewProps);
+                    child = child.display(locatedChildren.getCurrentState());
                 } else {
                     // les props n'ont pas été retransmise, cela veut dire que ce component n'existe plus
                     child = undefined;
@@ -107,46 +108,39 @@ class Component
             // On enlève les éventuelles undefined
             this.children = this.children.filter(child => !!child);
         }
-        return component;
+        return this;
     }
 
     /** Setters */
-    
-    getComponentId()
-    {
+
+    getComponentId() {
         return this.id;
     }
 
-    getCurrentState()
-    {
+    getCurrentState() {
         return this.currentState;
     }
 
-    getOldState()
-    {
+    getOldState() {
         return this.oldState
     }
 
     /** Getters */
-    
-    setCurrentState(currentState)
-    {
+
+    setCurrentState(currentState) {
         // Copie totalement l'objet currentState en créant un nouvel objet
         this.currentState = Object.assign({}, currentState);
     }
 
-    setOldState(oldState)
-    {
+    setOldState(oldState) {
         // Copie totalement l'objet currentState en créant un nouvel objet
         this.oldState = Object.assign({}, oldState);
     }
 }
 
 
-class State 
-{
-    constructor(idComponent, props, content)
-    {
+class State {
+    constructor(idComponent, props, content, children) {
         /** Id du component associé */
         var idComponent = idComponent;
 
@@ -156,31 +150,48 @@ class State
         /** Contenu du component associé */
         var content = content;
 
+        /** Enfant du component */
+        var children = children;
     }
 
-    getIdComponent()
-    {
+    getIdComponent() {
         return this.idComponent;
     }
 
-    getProps()
-    {
+    getProps() {
         return this.props;
     }
 
-    getContent()
-    {
+    getContent() {
         return this.content;
     }
 
-    setProps(props)
-    {
+    getChildren() {
+        return this.children();
+    }
+
+    setProps(props) {
         this.props = props;
     }
 
-    setContent(content)
-    {
+    setContent(content) {
         this.content = content;
+    }
+
+    /**
+    * Ajout un enfant au state
+    * @param Component enfant
+    */
+    addChild(child) {
+        this.children.push(child);
+    }
+
+    /**
+    * Supprime un enfant au state 
+    * @param Component enfant
+    */
+    removeChild(child) {
+        this.children = this.children.filter(currentChild => currentChild.getId() !== child.getId())
     }
 
 }
@@ -191,42 +202,37 @@ class State
  * @param array list liste de component
  * @returns le component ou undefined
  */
-function getComponentById(id, componentList)
-{
+function getComponentById(id, componentList) {
     return getFromListById(id, componentList);
 }
 
 /**
  * Retourne la props de la liste fourni s'il concorde avec l'id donnée
- * @param string id 
- * @param array list liste des props 
+ * @param string id
+ * @param array list liste des props
  * @returns le props correspondant ou undefined
  */
-function getPropsByComponentId(id, propsList)
-{
+function getPropsByComponentId(id, propsList) {
     return getFromListById(id, propsList);
 }
 
 /**
  * Retourne l'item de la liste fourni s'il concorde avec l'id donnée
- * @param string id 
+ * @param string id
  * @param array list liste de component ou liste de props
  * @returns un item de la liste concordant avec l'id  | un objet vide si rien n'a été trouvé
  */
-function getFromListById(id, list)
-{
+function getFromListById(id, list) {
     // Variable retournée
     let result = undefined;
 
     // Boucler sur la liste et examiner les items
-    for(let item of list)
-    {
+    for (let item of list) {
         // Examination de l'id de l'item selon le type de liste
-        if ((!!item.getId() && item.getID() === id)
-            || (!!item.getIdComponent() && item.getIdComponent() === id))
-        {
+        if ((!!item.getId() && item.getId() === id)
+            || (!!item.getIdComponent() && item.getIdComponent() === id)) {
             // Affectation si l'id est reconnu
-            result = item;    
+            result = item;
             //Arrêt de la boucle au premier trouvé
             break;
         }
@@ -234,8 +240,7 @@ function getFromListById(id, list)
     return result;
 }
 
-export
-{
+export {
     Component,
     Props,
     getComponentById,
